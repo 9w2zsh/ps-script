9d0da52a0e2244f99bd4ce70159956a53c80c54a
Zainal Azman Shaari <zainal.azman.shaari@quest.com>
2024-11-07 23:28:00 +0800

    Update QA365 lab to run multiple VM

--- RunLab.ps1
+++ RunLab.ps1
@@ -1,5 +1,11 @@
-﻿#Connect-AzAccount -Subscription "DevTest-BTDTS" >$null
-Set-AzContext -Subscription "DevTest-BTDTS" >$null
+﻿# define variables
+$labName = @("QA365", "QA2016", "QA365D12", "QA2019", "ZSMNE", "ZSJUMBOX")
+$qa365lab = @("QA365MNE1", "QA365MNE2", "QA365MNE3", "QA365DOM10M", "QA365DOM11C", "QA365BTUSR1", "QA365BTUSR2", "QA365BTUSR3", "QA365BTUSR4", "QA365SQL1", "QA365SQL2", "QA365BTMNCON2", "QA365BTMNCON3", "QA365BTMNWKR3", "QA365BTMNWKR5", "QA365BTMNWKR7", "QA365BTMNWKR8", "QA365BTMNWKR9", "QA365IIS")
+$qa365d12lab = @("QA365D12BTUSR1", "QA365D12BTUSR2", "QA365D12CON1", "QA365D12DOM12C", "QA365D12DOM12M", "QA365D12IIS1", "QA365D12MNE1", "QA365D12MNE2", "QA365D12MNE3", "QA365D12SQL1", "QA365D12SSDM1", "QA365D12SSDM2", "QA365D12WKR1", "QA365D12WKR2")
+$qa2016lab = @("QA2016DC1")
+$qa2019lab = @("QA2019CA1", "QA2019DC1", "QA2019BTMNCON", "QA2019BTMNWKR1", "QA2019DOM9C", "QA2019DOM9M", "QA2019EXCH1", "QA2019EXCH2", "QA2019IIS", "QA2019IIS2", "QA2019SQL1")
+$zsmnelab = @("ZSMNElab")
+$labInput = @()
 
 Write-Host "`n>>> Enter the LAB name to RUN and press [Enter] ..." -ForegroundColor Green
 if (!($lab = Read-Host "1: [QA365], `n2: [QA365D12], `n3: [QA2016], `n4: [QA2019], `n5: [ZSMNE], `n6: [JUMBOX] `n7: [DEV5] `nEnter number:")) { $lab = "1" }
@@ -8,132 +14,82 @@ if ($lab -eq "1") {
     $rg = "QA365-RG"
     $dc = "QA365DC1"
     $dom = "QA365DOM11M"
-    $mne1 = "QA365MNE1"
-    $mne2 = "QA365MNE3"
-    $exch = ""
-    $wks1 = "QA365BTUSR1"
-}
-elseif ($lab -eq "2") {
-    $rg = "QA365D12-RG"
-    $dc = "QA365D12DC1"
-    $dom = "QA365D12DOM12M"
-    $mne1 = "QA365D12MNE2"
-    $mne2 = "QA365D12MNE3"
-    $exch = ""
-    $wks1 = "QA365D12SSDM2"
-}
-elseif ($lab -eq "3") {
-    $rg = "QA2016-RG"
-    $dc = "QA2016DC1"
-    $dom = "QA2016DOM9M"
-    $mne1 = "QA2016CA1"
-    $mne2 = ""
-    $exch = "QA2016EXCH1"
-    $wks1 = ""
-}
-elseif ($lab -eq "4") {
-    $rg = "QA2019-RG"
-    $dc = "QA2019DC1"
-    $dom = "QA2019DOM9M"
-    $mne1 = "QA2019CA1"
-    $mne2 = "QA2019SQL1"
-    $exch = "QA2019EXCH2"
-    $wks1 = ""
-}
-elseif ($lab -eq "5") {
-    $rg = "ZSMNE"
-    $dc = ""
-    $dom = ""
-    $mne1 = "ZSMNElab"
-    $mne2 = ""
-    $exch = ""
-    $wks1 = ""
-    Set-AzContext -Subscription "QBU-PM.RD.SBox.MigratorNotesExchange" >$null
-}
-elseif ($lab -eq "6") {
-    $rg = "ZS"
-    $dc = ""
-    $dom = ""
-    $mne1 = "ZSjumbox"
-    $mne2 = ""
-    $exch = ""
-    $wks1 = ""
-    Set-AzContext -Subscription "QBU-PM.RD.SBox.MMAD" >$null
-}
-elseif ($lab -eq "7") {
-    $rg = "DEV5-RG"
-    $dc = "Dev5DC1"
-    $dom = "Dev5DOM9M"
-    $mne1 = "Dev5MNE4"
-    $mne2 = ""
-    $exch = ""
-    $wks1 = ""
-}
-else { Write-Output "`nLab not found, pls check lab name"; pause; return }
 
-if ($dc -ne "") {
+    for ($i = 0; $i -lt $qa365lab.Length; $i++) {
+        Write-Host "[$i]: $($qa365lab[$i])"
+    }
+
+    do {
+        $input = Read-Host "Enter VM to run and hit 'Enter' (type 'done' when finish)"
+        if ($input -ne "done") {
+            $labInput += $input
+        }
+    } until ($input -eq "done")
+
     $dcstat = Get-AzVm -Name $dc -Status | Select-Object Name, PowerState
     if ($dcstat.PowerState -eq "VM deallocated") {
         Write-Output "`nStarting $($dcstat.Name)"
         Start-AzVM -Name $dc -ResourceGroupName $rg
     }
     else { Write-Output "`n$($dcstat.Name) already running" }
-}
-else { Write-Output "No DC" }
 
-if ($dom -ne "") {
     $domstat = Get-AzVm -Name $dom -Status | Select-Object Name, PowerState
     if ($domstat.PowerState -eq "VM deallocated") {
         Write-Output "Starting $($domstat.Name)"
         Start-AzVM -Name $dom -ResourceGroupName $rg
     }
     else { Write-Output "$($domstat.Name) already running" }
-}
-else { Write-Output "No Domino server" }
 
-if ($mne1 -ne "") {
-    $mne1stat = Get-AzVm -Name $mne1 -Status | Select-Object Name, PowerState
-    if ($mne1stat.PowerState -eq "VM deallocated") {
-        Write-Output "Starting $($mne1stat.Name)"
-        Start-AzVM -Name $mne1 -ResourceGroupName $rg
+    for ($j = 0; $j -lt $labInput.Length; $j++) {
+        $currVM = $labInput[$j]
+        $vmName = $($qa365lab[$currVM])
+        $vmStat = Get-AzVm -Name $vmName -Status | Select-Object Name, PowerState
+        if ($vmStat.PowerState -eq "VM deallocated") {
+            Write-Output "`n$($vmStat.Name) not started. Starting now"
+            Start-AzVM -Name $vmName -ResourceGroupName $rg
+        }
+        else { Write-Output "$($vmStat.Name) already running" }
     }
-    else { Write-Output "$($mne1stat.Name) already running" }
+    pause
 }
-else { Write-Output "No MNE console" }
-
-if ($mne2 -ne "") {
-    $mne2stat = Get-AzVm -Name $mne2 -Status | Select-Object Name, PowerState
-    if ($mne2stat.PowerState -eq "VM deallocated") {
-        Write-Output "Starting $($mne2stat.Name)"
-        Start-AzVM -Name $mne2 -ResourceGroupName $rg
-    }
-    else { Write-Output "$($mne2stat.Name) already running" }
-}
-else { Write-Output "Only 1 MNE console" }
-
-if ($exch -ne "") {
-    $exchstat = Get-AzVm -Name $exch -Status | Select-Object Name, PowerState
-    if ($exchstat.PowerState -eq "VM deallocated") {
-        Write-Output "Starting $($exchstat.Name)"
-        Start-AzVM -Name $exch -ResourceGroupName $rg
-    }
-    else {
-        Write-Output "$($exchstat.Name) already running"
+elseif ($lab -eq "2") {
+    $dc = "QA365D12DC1"
+    $rg = "QA365D12-RG"
+    
+    for ($i = 0; $i -lt $qa365d12lab.Length; $i++) {
+        Write-Host "VM name $($qa365d12lab[$i])"
     }
 }
-else { Write-Output "No Exchange server" }
-
-if ($wks1 -ne "") {
-    $wks1stat = Get-AzVm -Name $wks1 -Status | Select-Object Name, PowerState
-    if ($wks1stat.PowerState -eq "VM deallocated") {
-        Write-Output "Starting $($wks1stat.Name)"
-        Start-AzVM -Name $wks1 -ResourceGroupName $rg
-    }
-    else {
-        Write-Output "$($wks1stat.Name) already running"
+elseif ($lab -eq "3") {
+    $dc = "QA2016DC1"
+    $rg = "QA2016-RG"
+    for ($i = 0; $i -lt $qa2016lab.Length; $i++) {
+        Write-Host "VM name $($qa2016lab[$i])"
     }
 }
-else { Write-Output "No workstation" }
+elseif ($lab -eq "4") {
+    $dc = "QA2019CA1"
+    $rg = "QA2019-RG"
 
-Write-Output "`n** END OF TASK **"
-pause
+    for ($i = 0; $i -lt $qa2019lab.Length; $i++) {
+        Write-Host "VM name $($qa2019lab[$i])"
+    }
+}
+elseif ($lab -eq "5") {
+    $rg = "ZSMNE"
+
+    for ($i = 0; $i -lt $zsmnelab.Length; $i++) {
+        Write-Host "VM name $($zsmnelab[$i])"
+    }
+}
+elseif ($lab -eq "6") {
+    for ($i = 0; $i -lt $zsjumbox.Length; $i++) {
+        Write-Host "VM name $($zsjumbox[$i])"
+    }
+}
+elseif ($lab -eq "7") {
+    for ($i = 0; $i -lt $dev5.Length; $i++) {
+        Write-Host "VM name $($dev5[$i])"
+    }
+}
+else { Write-Output "`nLab not found, pls check lab name"; pause; return }
