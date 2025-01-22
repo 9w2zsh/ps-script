b7bf73668a967b1a05e26bdf1d6ca8b9a9571eff
Zainal Azman Shaari <zainal.azman.shaari@quest.com>
2025-01-22 23:33:09 +0800

    2 labs from the menu

--- 0 RunVM.ps1
+++ 0 RunVM.ps1
@@ -1,11 +1,44 @@
-﻿$resgrp = "QA365-RG"
-$vmname = "QA365BTUSR1"
-$vmstat = Get-AzVm -Name $vmname -Status | Select-Object Name, PowerState
+﻿#Connect-AzAccount -Subscription "DevTest-BTDTS" >$null
+Set-AzContext -Subscription "DevTest-BTDTS" >$null
 
-if ($vmstat.PowerState -eq "VM deallocated"){
-    Write-Output "` ` Starting $($vmstat.Name)"
-    Start-AzVM -name $vmname -ResourceGroupName $resgrp
+Write-Host "`n>>> Enter the LAB name to RUN and press [Enter] ..." -ForegroundColor Green
+if (!($lab = Read-Host "1: [QA365], `n2: [QA365D12], `n3: [QA2016], `n4: [QA2019], `n5: [ZSMNE], `n6: [JUMBOX] `n7: [DEV5] `nEnter number:")) { $lab = "1" }
+
+if ($lab -eq "1") {
+    $rg = "QA365-RG"
+    $dc = "QA365DC1"
+    $vms = Get-AzVM -ResourceGroupName $rg
+    $dcstat = Get-AzVM -Name $dc -Status | Select-Object Name, PowerState
+    if ($dcstat.PowerState -eq "VM deallocated") {
+        Write-Output "`nStarting $($dc)"
+        Start-AzVM -Name $dc -ResourceGroupName $rg
+    }
+    else { Write-Output "`n$($dc) already running" }
 }
-else {
-    Write-Output "` ` $($vmstat.Name) already running"
+elseif ($lab -eq "2") {
+    $rg = "QA365D12-RG"
+    $dc = "QA365D12DC1"
+    $vms = Get-AzVM -ResourceGroupName $rg
+    $dcstat = Get-AzVM -Name $dc -Status | Select-Object Name, PowerState
+    if ($dcstat.PowerState -eq "VM deallocated") {
+        Write-Output "`nStarting $($dc)"
+        Start-AzVM -Name $dc -ResourceGroupName $rg
+    }
+    else { Write-Output "`n$($dc) already running" }
 }
+else { Write-Output "`nLab not found, pls check lab name"; pause; return }
+    
+foreach ($vm in $vms) {
+    $vmname = Get-AzVM -Name $vm.Name -Status | Select-Object Name, PowerState
+    if ($vmname.PowerState -eq "VM deallocated") {
+        Write-Output "`n$($vm.Name), $($vmname.PowerState)"
+        if (!($runvm = Read-Host "Run VM?: ")) { $runvm = "N" }
+        if ($runvm -eq "Y"){
+            Write-Output "Starting $($vm.Name)"
+            Start-AzVM -Name $vm.Name -ResourceGroupName $rg -NoWait
+        }
+    }
+    else { Write-Output "`n$($vm.Name) already running" }
+}
+
+Pause
