d3dd4dea684cc9a8bac2737c1f3a5d1061e33ddd
Zainal Azman Shaari <zainal.azman.shaari@quest.com>
2025-01-23 10:47:43 +0800

    update db name and remove files in CommonTools

--- Uninstall MNE.ps1
+++ Uninstall MNE.ps1
@@ -1,15 +1,18 @@
 Uninstall-Package -Name "Quest Migrator for Notes to Exchange"
-Write-Host "`nMNE deleted..." -ForegroundColor Green
+Write-Host "`nMNE deleted..." -ForegroundColor Yellow
 Start-Sleep -Milliseconds 3000
 
-$currentDir=$pwd
+Remove-Item "C:\MNE\CommonTools\*.*"
+Write-Host "`nFiles in CommonTools deleted..." -ForegroundColor Yellow
+
+$currentDir = $pwd
 $hostname = hostname
-$SqlServer = $hostname + "\QA365D12MNE2"
+$SqlServer = $hostname + "\SQLExpress"
 
 $DbName = Invoke-Sqlcmd -ServerInstance $SqlServer -Query "SELECT name FROM master.dbo.sysdatabases WHERE name = 'NME40DB'"
 if ($null -ne $DbName) {
-	Invoke-Sqlcmd -ServerInstance $SqlServer -Query "Alter database [NME40DB] set single_user with rollback immediate; USE [master]; DROP DATABASE [NME40DB]" 
-    Write-Host "`nMNE database deleted..." -ForegroundColor Green
+    Invoke-Sqlcmd -ServerInstance $SqlServer -Query "Alter database [NME40DB] set single_user with rollback immediate; USE [master]; DROP DATABASE [NME40DB]" 
+    Write-Host "`nMNE database deleted..." -ForegroundColor Yellow
 }
 
 pause
